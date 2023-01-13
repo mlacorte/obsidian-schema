a3693888bad665f890e905d60f2436e37de5be8a
Michael LaCorte <michael@mlacorte.com>
2023-01-13 16:13:59 -0600

    Add cross-type unions

--- __tests__/types.ts
+++ __tests__/types.ts
@@ -1,17 +1,43 @@
-import * as T from "../src/types";
+import { Number, Union } from "../src/types";
 
 describe("types", () => {
-  test("numbers", () => {
-    const all = T.Number;
-    const one = T.Number.literal(1);
-    const two = T.Number.literal(2);
-    const none = one.intersect(two);
-    const both = one.union(two);
+  test("types", () => {
+    const all = Number;
+    const one = Number.literal(1);
+    const two = Number.literal(2);
+    const none = one.and(two);
+    const both = one.or(two);
 
     expect(all.size).toBe(-1);
     expect(one.size).toBe(1);
     expect(two.size).toBe(1);
     expect(none.size).toBe(0);
     expect(both.size).toBe(2);
+  });
+
+  test("unions", () => {
+    const number = Union.type("number");
+    const one = Union.literal("number", 1);
+    const string = Union.type("string");
+    const a = Union.literal("string", "a");
+
+    expect(number.size).toBe(1);
+    expect(one.size).toBe(1);
+    expect(string.size).toBe(1);
+    expect(a.size).toBe(1);
+
+    const typesOr = number.or(string);
+    const typesAnd = number.and(string);
+    const litsOr = one.or(a);
+    const litsAnd = one.and(a);
+    const allOr = typesOr.or(litsOr);
+    const allAnd = typesAnd.and(litsAnd);
+
+    expect(typesOr.size).toBe(2);
+    expect(typesAnd.size).toBe(0);
+    expect(litsOr.size).toBe(2);
+    expect(litsAnd.size).toBe(0);
+    expect(allOr.size).toBe(2);
+    expect(allAnd.size).toBe(0);
   });
 });
--- src/types.ts
+++ src/types.ts
@@ -1,96 +1,278 @@
-import {
-  DateTime,
-  Duration as _Duration,
-  Failure,
-  Success
-} from "obsidian-dataview";
+import * as L from "luxon";
+import { Failure, Success } from "obsidian-dataview";
 import { EXPRESSION, Result } from "obsidian-dataview";
 import * as Parsimmon from "parsimmon";
 
 type TypeMap = {
-  null: { type: null; serialize: null; valueOf: null };
-  number: { type: number; serialize: number; valueOf: number };
-  string: { type: string; serialize: string; valueOf: string };
-  boolean: { type: boolean; serialize: boolean; valueOf: boolean };
-  date: { type: DateTime; serialize: string; valueOf: number };
-  duration: { type: _Duration; serialize: string; valueOf: number };
+  null: { type: null; serialize: null; unlifted: null };
+  number: { type: number; serialize: number; unlifted: number };
+  string: { type: string; serialize: string; unlifted: string };
+  boolean: { type: boolean; serialize: boolean; unlifted: boolean };
+  date: { type: L.DateTime; serialize: string; unlifted: number };
+  duration: { type: L.Duration; serialize: string; unlifted: number };
 };
+
+const ANY: unique symbol = Symbol("*");
 
 type Types = keyof TypeMap;
 type TypeOf<T extends Types> = TypeMap[T]["type"];
-type SerializeTypeOf<T extends Types> = TypeMap[T]["serialize"];
-type ValueTypeOf<T extends Types> = TypeMap[T]["valueOf"];
+type Serialize<T extends Types> = TypeMap[T]["serialize"];
+type Unlifted<T extends Types> = TypeMap[T]["unlifted"];
 
 interface TypeFns<T extends Types> {
-  serialize(val: TypeOf<T>): SerializeTypeOf<T>;
+  serialize(val: TypeOf<T>): Serialize<T>;
   deserialize(val: any): Result<TypeOf<T>, string>;
-  valueOfFn(val: TypeOf<T>): ValueTypeOf<T>;
+  lift(val: Unlifted<T>): TypeOf<T>;
+  unlift(val: TypeOf<T>): Unlifted<T>;
 }
 
-class Type<T extends Types> {
-  static declare<T extends Types>(type: T, fns: TypeFns<T>): Type<T> {
-    return new Type<T>(
-      type,
-      "*",
-      fns.serialize,
-      fns.deserialize,
-      fns.valueOfFn
-    );
+export class Union {
+  private static readonly defaults = {
+    null: null,
+    number: null,
+    string: null,
+    boolean: null,
+    date: null,
+    duration: null
+  };
+
+  private static typeKeys = Object.keys(
+    Union.defaults
+  ) as (keyof typeof Union.defaults)[];
+
+  public readonly type = "union";
+  public readonly types: { [T in Types]: Type<T> | null };
+
+  private constructor(values: { [T in Types]?: Type<T> } = {}) {
+    this.types = { ...Union.defaults, ...values };
   }
 
-  private constructor(
-    public readonly type: T,
-    public readonly values: "*" | ReadonlySet<ValueTypeOf<T>>,
-    public readonly serialize: (val: TypeOf<T>) => SerializeTypeOf<T>,
-    public readonly deserialize: (val: any) => Result<TypeOf<T>, string>,
-    public readonly valueOfFn: (val: TypeOf<T>) => ValueTypeOf<T>
-  ) {}
+  static literal<T extends Types>(key: T, value: TypeOf<T>): Union {
+    const lit = {
+      null: (val: null) => Null.literal(val),
+      number: (val: number) => Number.literal(val),
+      string: (val: string) => String.literal(val),
+      boolean: (val: boolean) => Boolean.literal(val),
+      date: (val: L.DateTime) => Date.literal(val),
+      duration: (val: L.Duration) => Duration.literal(val)
+    }[key] as (value: TypeOf<T>) => Type<T>;
 
-  literal(val: TypeOf<T>): Type<T> {
-    return this.clone([val]);
+    return new Union({ [key]: lit(value) });
   }
 
-  union(other: Type<T>): Type<T> {
-    const lVals = this.values;
-    const rVals = other.values;
+  static type<T extends Types>(key: T): Union {
+    const type = {
+      null: Null,
+      number: Number,
+      string: String,
+      boolean: Boolean,
+      date: Date,
+      duration: Duration
+    }[key] as Type<T>;
 
-    if (lVals === "*" || rVals === "*") {
-      return this.clone("*");
-    }
-
-    return this.clone([...lVals, ...rVals]);
+    return new Union({ [key]: type });
   }
 
-  intersect(other: Type<T>): Type<T> {
-    const lVals = this.values;
-    const rVals = other.values;
+  or(other: Union): Union {
+    const values: any = {};
 
-    if (lVals === "*") {
-      return this.clone(rVals);
+    this.iterate(other, (type, l, r) => {
+      if (l && r) {
+        values[type] = l.or(r);
+      } else if (l) {
+        values[type] = l;
+      } else if (r) {
+        values[type] = r;
+      }
+    });
+
+    return new Union(values);
+  }
+
+  and(other: Union): Union {
+    const values: any = {};
+
+    this.iterate(other, (type, l, r) => {
+      if (l && r) {
+        const res = l.and(r);
+
+        if (res.size !== 0) {
+          values[type] = res;
+        }
+      }
+    });
+
+    return new Union(values);
+  }
+
+  // iterates over types
+  *keys() {
+    for (const [key, value] of this.entries()) {
+      if (value === ANY) {
+        yield key;
+      }
     }
+  }
 
-    if (rVals === "*") {
-      return this.clone(lVals);
+  // iterates over literals
+  *values() {
+    for (const [_, value] of this.entries()) {
+      if (value !== ANY) {
+        yield value;
+      }
     }
+  }
 
-    return this.clone([...lVals].filter((lVal) => rVals.has(lVal)));
+  // iterates over types and literals
+  *entries() {
+    for (const key of Union.typeKeys) {
+      const type = this.types[key];
+
+      if (!type) {
+        continue;
+      }
+
+      for (const value of type.values()) {
+        yield [key, value] as {
+          [K in keyof TypeMap]: [K, typeof ANY | TypeOf<K>];
+        }[keyof TypeMap];
+      }
+    }
+  }
+
+  forEach(
+    fn: <T extends Types>(
+      value: typeof ANY | TypeOf<T>,
+      key: T,
+      union: Union
+    ) => void
+  ) {
+    for (const [key, value] of this.entries()) {
+      fn(value, key, this);
+    }
   }
 
   get size() {
-    return this.values === "*" ? -1 : this.values.size;
+    let res = 0;
+
+    for (const type of Union.typeKeys) {
+      const val = this.types[type];
+
+      if (val) {
+        res += Math.abs(val.size);
+      }
+    }
+
+    return res;
   }
 
-  private clone(values: "*" | TypeOf<T>[] | ReadonlySet<ValueTypeOf<T>>) {
+  private iterate(
+    other: Union,
+    fn: <T extends Types>(type: T, l: Type<T> | null, r: Type<T> | null) => void
+  ) {
+    for (const type of Union.typeKeys) {
+      fn(type, this.types[type], other.types[type]);
+    }
+  }
+}
+
+export class Type<T extends Types> implements TypeFns<T> {
+  static declare<T extends Types>(type: T, fns: TypeFns<T>): Type<T> {
+    return new Type<T>(type, ANY, fns);
+  }
+
+  readonly serialize = this.fns.serialize;
+  readonly deserialize = this.fns.deserialize;
+  readonly lift = this.fns.lift;
+  readonly unlift = this.fns.unlift;
+
+  private constructor(
+    public readonly type: T,
+    public readonly literals: typeof ANY | ReadonlyMap<Unlifted<T>, TypeOf<T>>,
+    private readonly fns: TypeFns<T>
+  ) {}
+
+  literal(val: TypeOf<T>): Type<T> {
+    if (this === undefined) {
+      throw new Error("wtf?");
+    }
+
+    return this.clone([val]);
+  }
+
+  or(other: Type<T>): Type<T> {
+    const lVals = this.literals;
+    const rVals = other.literals;
+
+    if (lVals === ANY || rVals === ANY) {
+      return this.clone(ANY);
+    }
+
+    return this.clone([...lVals.keys(), ...rVals.keys()]);
+  }
+
+  and(other: Type<T>): Type<T> {
+    const lVals = this.literals;
+    const rVals = other.literals;
+
+    if (lVals === ANY) {
+      return this.clone(rVals);
+    }
+
+    if (rVals === ANY) {
+      return this.clone(lVals);
+    }
+
+    return this.clone([...lVals.keys()].filter((lVal) => rVals.has(lVal)));
+  }
+
+  keys() {
+    return this.values();
+  }
+
+  *values() {
+    if (this.literals === ANY) {
+      yield ANY;
+    } else {
+      for (const value of this.literals.values()) {
+        yield value;
+      }
+    }
+  }
+
+  *entries() {
+    for (const value of this.values()) {
+      yield [value, value] as const;
+    }
+  }
+
+  forEach(
+    fn: (
+      value: typeof ANY | TypeOf<T>,
+      key: typeof ANY | TypeOf<T>,
+      types: Type<T>
+    ) => void
+  ) {
+    for (const value of this.values()) {
+      fn(value, value, this);
+    }
+  }
+
+  get size() {
+    return this.literals === ANY ? -1 : this.literals.size;
+  }
+
+  private clone(
+    values: typeof ANY | TypeOf<T>[] | ReadonlyMap<Unlifted<T>, TypeOf<T>>
+  ) {
     return new Type<T>(
       this.type,
-      values === "*"
+      values === ANY
         ? values
         : Array.isArray(values)
-        ? new Set(values.map((v) => this.valueOfFn(v)))
-        : new Set(values),
-      this.serialize,
-      this.deserialize,
-      this.valueOfFn
+        ? new Map(values.map((v) => [this.unlift(v), v]))
+        : new Map(values),
+      this.fns
     );
   }
 }
@@ -98,10 +280,11 @@ class Type<T extends Types> {
 export const Null = Type.declare("null", {
   serialize: (val) => val,
   deserialize: (val) =>
-    val === null
-      ? new Success(val)
+    val === null || val === undefined
+      ? new Success(null)
       : new Failure(`"${val}" not of type "null"`),
-  valueOfFn: (val) => val
+  lift: (val) => val,
+  unlift: (val) => val
 });
 
 export const Number = Type.declare("number", {
@@ -110,7 +293,8 @@ export const Number = Type.declare("number", {
     typeof val === "number"
       ? new Success(val)
       : new Failure(`"${val}" not of type "number"`),
-  valueOfFn: (val) => val
+  lift: (val) => val,
+  unlift: (val) => val
 });
 
 export const String = Type.declare("string", {
@@ -119,7 +303,8 @@ export const String = Type.declare("string", {
     typeof val === "string"
       ? new Success(val)
       : new Failure(`"${val}" not of type "string"`),
-  valueOfFn: (val) => val
+  lift: (val) => val,
+  unlift: (val) => val
 });
 
 export const Boolean = Type.declare("boolean", {
@@ -128,7 +313,8 @@ export const Boolean = Type.declare("boolean", {
     typeof val === "boolean"
       ? new Success(val)
       : new Failure(`"${val}" not of type "boolean"`),
-  valueOfFn: (val) => val
+  lift: (val) => val,
+  unlift: (val) => val
 });
 
 export const Date = Type.declare("date", {
@@ -139,7 +325,8 @@ export const Date = Type.declare("date", {
       ? new Success(res.value)
       : new Failure(Parsimmon.formatError(val, res));
   },
-  valueOfFn: (val) => val.valueOf()
+  lift: (val) => L.DateTime.fromMillis(val),
+  unlift: (val) => val.toMillis()
 });
 
 export const Duration = Type.declare("duration", {
@@ -150,5 +337,6 @@ export const Duration = Type.declare("duration", {
       ? new Success(res.value)
       : new Failure(Parsimmon.formatError(val, res));
   },
-  valueOfFn: (val) => val.valueOf()
+  lift: (val) => L.Duration.fromMillis(val),
+  unlift: (val) => val.toMillis()
 });
