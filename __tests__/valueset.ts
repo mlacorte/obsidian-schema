eb5d823b556e928d3b2cc12152936757f4b069ad
Michael LaCorte <michael@mlacorte.com>
2023-03-20 14:26:29 -0500

    Remove use of "$" utility in tests

--- __tests__/valueset.ts
+++ __tests__/valueset.ts
@@ -1,73 +1,86 @@
-import { $ } from "../src/types";
+import * as T from "../src/types";
 import { fn, val } from "../src/valueset";
+
+const one = T.Number.literal(1);
+const two = T.Number.literal(2);
+const three = T.Number.literal(3);
+const four = T.Number.literal(4);
+const five = T.Number.literal(5);
+const six = T.Number.literal(6);
 
 describe("valueset", () => {
   test("valueset", () => {
-    const empty = fn("empty", [], () => $(1));
+    const empty = fn("empty", [], () => one);
 
-    expect(empty.values()).toEqual([$(1)]);
+    expect(empty.values()).toEqual([one]);
     expect(empty.conditions()).toEqual([[]]);
 
-    const a = val("a", [$(1), $(2)]);
+    const a = val("a", [one, two]);
 
-    expect(a.values()).toEqual([$(1), $(2)]);
-    expect(a.conditions()).toEqual([[[a.id, $(1)]], [[a.id, $(2)]]]);
+    expect(a.values()).toEqual([one, two]);
+    expect(a.conditions()).toEqual([[[a.id, one]], [[a.id, two]]]);
 
-    const b = val("b", [$(1), $(2)]);
+    const b = val("b", [one, two]);
 
-    expect(b.values()).toEqual([$(1), $(2)]);
-    expect(b.conditions()).toEqual([[[b.id, $(1)]], [[b.id, $(2)]]]);
+    expect(b.values()).toEqual([one, two]);
+    expect(b.conditions()).toEqual([[[b.id, one]], [[b.id, two]]]);
 
-    const c = fn("c", [a, b], (a, b) => $((a.value as any) + b.value));
+    const c = fn("c", [a, b], (a, b) =>
+      T.Number.literal((a.value as any) + b.value)
+    );
 
-    expect(c.values()).toEqual([$(2), $(3), $(3), $(4)]);
+    expect(c.values()).toEqual([two, three, three, four]);
     expect(c.conditions()).toEqual([
       [
-        [a.id, $(1)],
-        [b.id, $(1)]
+        [a.id, one],
+        [b.id, one]
       ],
       [
-        [a.id, $(1)],
-        [b.id, $(2)]
+        [a.id, one],
+        [b.id, two]
       ],
       [
-        [a.id, $(2)],
-        [b.id, $(1)]
+        [a.id, two],
+        [b.id, one]
       ],
       [
-        [a.id, $(2)],
-        [b.id, $(2)]
+        [a.id, two],
+        [b.id, two]
       ]
     ]);
 
-    const d = fn("d", [a, a], (l, r) => $((l.value as any) + r.value));
+    const d = fn("d", [a, a], (l, r) =>
+      T.Number.literal((l.value as any) + r.value)
+    );
 
-    expect(d.values()).toEqual([$(2), $(4)]);
-    expect(d.conditions()).toEqual([[[a.id, $(1)]], [[a.id, $(2)]]]);
+    expect(d.values()).toEqual([two, four]);
+    expect(d.conditions()).toEqual([[[a.id, one]], [[a.id, two]]]);
 
-    const e = fn("e", [a, c], (a, c) => $((c.value as any) + a.value));
+    const e = fn("e", [a, c], (a, c) =>
+      T.Number.literal((c.value as any) + a.value)
+    );
 
-    expect(e.values()).toEqual([$(3), $(4), $(5), $(6)]);
+    expect(e.values()).toEqual([three, four, five, six]);
     expect(e.conditions()).toEqual([
       [
-        [a.id, $(1)],
-        [b.id, $(1)],
-        [c.id, $(2)]
+        [a.id, one],
+        [b.id, one],
+        [c.id, two]
       ],
       [
-        [a.id, $(1)],
-        [b.id, $(2)],
-        [c.id, $(3)]
+        [a.id, one],
+        [b.id, two],
+        [c.id, three]
       ],
       [
-        [a.id, $(2)],
-        [b.id, $(1)],
-        [c.id, $(3)]
+        [a.id, two],
+        [b.id, one],
+        [c.id, three]
       ],
       [
-        [a.id, $(2)],
-        [b.id, $(2)],
-        [c.id, $(4)]
+        [a.id, two],
+        [b.id, two],
+        [c.id, four]
       ]
     ]);
   });
