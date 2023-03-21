45f830e7c0fc847158491291d0e576cee595a496
Michael LaCorte <michael@mlacorte.com>
2023-03-21 13:16:30 -0500

    Improve builtin typings

--- src/builtins.ts
+++ src/builtins.ts
@@ -2,42 +2,42 @@ import { Widgets } from "obsidian-dataview";
 
 import * as T from "./types";
 
-export const op: Record<string, T.FunctionType> = {
-  lte: T.Function.define("lte", [])
-    .add([T.Any, T.Any], () => T.Never.error("TODO: implement 'lte'"))
-    .build(),
+const lte = T.Function.define("lte", [])
+  .add([T.Any, T.Any], () => T.Never.error("TODO: implement 'lte'"))
+  .build();
 
-  gt: T.Function.define("gt", [])
-    .add([T.Any, T.Any], () => T.Never.error("TODO: implement 'gt'"))
-    .build(),
+const gt = T.Function.define("gt", [])
+  .add([T.Any, T.Any], () => T.Never.error("TODO: implement 'gt'"))
+  .build();
 
-  gte: T.Function.define("gte", [])
-    .add([T.Any, T.Any], () => T.Never.error("TODO: implement 'gte'"))
-    .build(),
+const gte = T.Function.define("gte", [])
+  .add([T.Any, T.Any], () => T.Never.error("TODO: implement 'gte'"))
+  .build();
 
-  eq: T.Function.define("eq", [])
-    .add([T.Any, T.Any], () => T.Never.error("TODO: implement 'eq'"))
-    .build(),
+const eq = T.Function.define("eq", [])
+  .add([T.Any, T.Any], () => T.Never.error("TODO: implement 'eq'"))
+  .build();
 
-  neq: T.Function.define("neq", [])
-    .add([T.Any, T.Any], () => T.Never.error("TODO: implement 'neq'"))
-    .build()
-};
+const neq = T.Function.define("neq", [])
+  .add([T.Any, T.Any], () => T.Never.error("TODO: implement 'neq'"))
+  .build();
 
-export const fn: Record<string, T.FunctionType> = {
-  choice: T.Function.define("choice", [0, 1, 2])
-    .add(
-      [T.Boolean, T.Any, T.Any],
-      (cond: T.BooleanType, pass: T.Type, fail: T.Type) =>
-        cond.isType() ? pass.or(fail) : cond.value ? pass : fail
-    )
-    .build(),
+export const op = { lte, gt, gte, eq, neq };
 
-  elink: T.Function.define("elink", [0])
-    .add([T.String, T.String], T.Link, [0, 1], (a: string, d: string) =>
-      T.Widget.literal(Widgets.externalLink(a, d))
-    )
-    .add([T.String, [T.Null]], (s: T.StringType) => fn.elink.eval(s, s))
-    .add([T.Null, [T.Any]], () => T.Null)
-    .build()
-};
+const choice = T.Function.define("choice", [0, 1, 2])
+  .add(
+    [T.Boolean, T.Any, T.Any],
+    (cond: T.BooleanType, pass: T.Type, fail: T.Type) =>
+      cond.isType() ? pass.or(fail) : cond.value ? pass : fail
+  )
+  .build();
+
+const elink: T.FunctionType = T.Function.define("elink", [0])
+  .add([T.String, T.String], T.Link, [0, 1], (a: string, d: string) =>
+    T.Widget.literal(Widgets.externalLink(a, d))
+  )
+  .add([T.String, [T.Null]], (s: T.StringType) => elink.eval(s, s))
+  .add([T.Null, [T.Any]], () => T.Null)
+  .build();
+
+export const fn = { choice, elink };
