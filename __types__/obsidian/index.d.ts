da3565e404ced5e806082eb11607e513513a57ac
Michael LaCorte <michael@mlacorte.com>
2023-03-23 15:17:13 -0500

    Enahnced event typing

--- __types__/obsidian/index.d.ts
+++ __types__/obsidian/index.d.ts
@@ -1,8 +1,39 @@
 import "obsidian-dataview/lib/typings/workers";
 
+// https://github.com/microsoft/TypeScript/issues/32164#issuecomment-1146737709
+type OverloadProps<TOverload> = Pick<TOverload, keyof TOverload>;
+type OverloadUnionRecursive<
+  TOverload,
+  TPartialOverload = unknown
+> = TOverload extends (...args: infer TArgs) => infer TReturn
+  ? TPartialOverload extends TOverload
+    ? never
+    :
+        | OverloadUnionRecursive<
+            TPartialOverload & TOverload,
+            TPartialOverload &
+              ((...args: TArgs) => TReturn) &
+              OverloadProps<TOverload>
+          >
+        | ((...args: TArgs) => TReturn)
+  : never;
+type OverloadUnion<TOverload extends (...args: any[]) => any> = Exclude<
+  OverloadUnionRecursive<(() => never) & TOverload>,
+  TOverload extends () => never ? never : () => never
+>;
+
 declare module "obsidian" {
   interface MetadataCache {
-    trigger(...args: Parameters<MetadataCache["on"]>): void;
-    trigger(name: string, ...data: any[]): void;
+    on(name: "dataview:index-ready", callback: () => any): EventRef;
+    on(
+      name: "dataview:metadata-change",
+      callback: <T extends "update" | "delete" | "rename">(
+        type: T,
+        file: TFile,
+        oldPath: T extends "rename" ? string : undefined
+      ) => any
+    ): EventRef;
+
+    trigger(...args: Parameters<OverloadUnion<MetadataCache["on"]>>): void;
   }
 }
