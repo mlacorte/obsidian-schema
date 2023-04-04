d1db6e89fb1c80f9d9e0da4d58f345752717fd52
Michael LaCorte <michael@mlacorte.com>
2023-04-04 17:45:41 -0500

    Parser utility enhancements

--- src/parser.ts
+++ src/parser.ts
@@ -4,24 +4,25 @@ import { ParserConfig } from "@lezer/lr";
 import { parser } from "./parser/schema.parser";
 
 export function parse(str: string, config?: ParserConfig): Tree {
-  const parserInstance = config ? parser.configure(config) : parser;
+  return parser.configure(config || {}).parse(str);
+}
 
-  return parserInstance.parse(str);
+export function* traverse(tree: Tree) {
+  const cursor = tree.cursor();
+
+  do {
+    yield cursor;
+  } while (cursor.next());
 }
 
 export function hasErrors(tree: Tree) {
-  let error = false;
-
-  tree.iterate({
-    enter: (ref) => {
-      if (ref.type.isError) {
-        error = true;
-        return false;
-      }
+  for (const cursor of traverse(tree)) {
+    if (cursor.type.isError) {
+      return true;
     }
-  });
+  }
 
-  return error;
+  return false;
 }
 
 export function children(tree: Tree | SyntaxNode): SyntaxNode[] {
@@ -38,9 +39,7 @@ export function children(tree: Tree | SyntaxNode): SyntaxNode[] {
   return nodes;
 }
 
-type ParseTreeJSON =
-  | [string, string]
-  | [string, ParseTreeJSON, ...ParseTreeJSON[]];
+type ParseTreeJSON = [string, string] | [string, ...ParseTreeJSON[]];
 
 export function toJSON(str: string, tree: Tree): ParseTreeJSON {
   const toJSONRec = (node: SyntaxNode): ParseTreeJSON => {
@@ -55,6 +54,39 @@ export function toJSON(str: string, tree: Tree): ParseTreeJSON {
   };
 
   return toJSONRec(tree.topNode);
+}
+
+export function prettyPrint(str: string, tree?: Tree): string {
+  const strs: string[] = [];
+
+  const printASTRec = ([name, ...values]: ParseTreeJSON, depth: number) => {
+    const indent = " ".repeat(depth * 2);
+    name = JSON.stringify(name);
+
+    if (typeof values[0] === "string") {
+      const value = JSON.stringify(values[0]);
+
+      if (value.length === 0) {
+        strs.push(`${indent}[${name}, []],`);
+      } else {
+        strs.push(`${indent}[${name}, ${value}],`);
+      }
+    } else {
+      strs.push(`${indent}[${name}, [`);
+
+      for (const child of values as ParseTreeJSON[]) {
+        printASTRec(child, depth + 1);
+      }
+
+      strs.push(`${indent}],`);
+    }
+  };
+
+  printASTRec(toJSON(str, tree || parse(str)), 0);
+
+  const res = strs.join("\n").slice(0, -1);
+
+  return res;
 }
 
 export { parser };
