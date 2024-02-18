2c1665a5505d63d42022c3a532a1036d6791f12d
Michael LaCorte <michael@mlacorte.com>
2024-02-18 00:28:27 -0600

    Progress on typeset

--- packages/schema/src/typeset.ts
+++ packages/schema/src/typeset.ts
@@ -1,23 +1,67 @@
-export type IUnionMap = {
-  any: null;
-  never: string[];
-  union: IValue;
-};
-export type IValueMap = {
+export type ITypeMap = {
   null: null;
   number: null | number;
   string: null | string;
   array: { known: IType[]; unknown: IType };
+  object: { known: Map<string, IType>; unknown: IType };
 };
-export type ITypeMap = IUnionMap & IValueMap;
+export type IType = ["any"] | ["never", ...string[]] | IVal[];
+export type IVal = {
+  [K in keyof ITypeMap]: [K, ...ITypeMap[K][]];
+}[keyof ITypeMap];
 
-export type IUnion = {
-  [K in keyof IUnionMap]: [K, ITypeMap[K]];
-}[keyof IUnionMap];
-export type IValue = { [K in keyof IValueMap]: ITypeOf<K> }[keyof IValueMap];
-export type IType = IUnion | IValue;
+export const TypeFns = {
+  or(as: IType, bs: IType): IType {
+    // any
+    if (as[0] === "any" || bs[0] === "any") return as;
 
-export type ITypeOf<K extends keyof ITypeMap> = [K, ...ITypeMap[K][]];
+    // never
+    if (as[0] === "never" && bs[0] === "never") {
+      return ["never", ...NeverFns.or(as.slice(1), bs.slice(1))];
+    }
+    if (as[0] === "never") return bs;
+    if (bs[0] === "never") return as;
+
+    // vals
+    return UnionFns.or(as, bs);
+  },
+};
+
+export const UnionFns = {
+  or(as: IVal[], bs: IVal[]): IVal[] {
+    return [...UtilFns.union<IVal>(as, bs, ValFns.or, ValFns.compare)];
+  },
+  compare(as: IVal[], bs: IVal[]): number {
+    return UtilFns.compare(as, bs, ValFns.compare);
+  },
+};
+
+export const ValFns = {
+  or(a: IVal, b: IVal): [IVal] | [IVal, IVal] {
+    if (a[0] === b[0]) {
+      const { or, compare } = fns(a[0]);
+      return [
+        [a[0], ...UtilFns.union(a.slice(0), b.slice(0), or, compare)] as IVal,
+      ];
+    } else {
+      return StringFns._compare(a[0], b[0]) < 0 ? [a, b] : [b, a];
+    }
+  },
+  compare(a: IVal, b: IVal): number {
+    const typeSort = StringFns._compare(a[0], b[0]);
+    if (typeSort !== 0) return typeSort;
+    return UtilFns.compare(a.slice(1), b.slice(1), fns(a[0]).compare);
+  },
+};
+
+export const NeverFns = {
+  or(as: string[], bs: string[]): string[] {
+    return [...UtilFns.union(as, bs, StringFns._or, StringFns._compare)];
+  },
+  intersect(as: string[], bs: string[]): string[] {
+    return [...UtilFns.intersect(as, bs, StringFns._and, StringFns._compare)];
+  },
+};
 
 type IFnsType<T> = {
   or(a: T, b: T): [T] | [T, T];
@@ -30,97 +74,12 @@ type IFnsType<T> = {
 
 type IFns<K extends keyof ITypeMap> = IFnsType<ITypeMap[K]>;
 
-export const head = <K extends keyof ITypeMap>(type: ITypeOf<K>) => type[0];
-export const tail = <K extends keyof ITypeMap>(type: ITypeOf<K>) =>
-  type.slice(1) as ITypeMap[K][];
-export const split = <K extends keyof ITypeMap>(type: ITypeOf<K>) =>
-  [head(type), tail(type)] as const;
+const baseFns = <K extends keyof ITypeMap>(fn: () => IFns<K>) => fn();
 
-export const TypeFns = {
-  union(a: IUnion, b: IUnion): IUnion {
-    if (a[0] === "any" || b[0] === "any") return a;
-    if (a[0] === "never" && b[0] === "never") {
-      return ["never", NeverFns.or(a[1], b[1])[0]];
-    }
-    if (a[0] === "never") return b;
-    if (b[0] === "never") return a;
-
-    throw "TODO";
-  },
-};
-
-export const UtilFns = {
-  *intersect<V>(
-    as: V[],
-    bs: V[],
-    and: (a: V, b: V) => [] | [V],
-    compare: (a: V, b: V) => number,
-  ) {
-    let ai = 0;
-    let bi = 0;
-
-    while (ai < as.length && bi < bs.length) {
-      const a = as[ai];
-      const b = bs[bi];
-      const intersection = and(a, b);
-
-      // merged
-      if (intersection.length === 1) {
-        yield intersection[0];
-        ai++;
-        bi++;
-        continue;
-      }
-
-      // disjoint
-      if (compare(a, b) <= 0) {
-        ai++;
-      } else {
-        bi++;
-      }
-    }
-  },
-  *union<V>(
-    as: V[],
-    bs: V[],
-    or: (a: V, b: V) => [V] | [V, V],
-    compare: (a: V, b: V) => number,
-  ) {
-    let ai = 0;
-    let bi = 0;
-
-    while (ai < as.length && bi < bs.length) {
-      const a = as[ai];
-      const b = bs[bi];
-      const union = or(a, b);
-
-      // merged
-      if (union.length === 1) {
-        yield union[0];
-        ai++;
-        bi++;
-        continue;
-      }
-
-      // disjoint
-      if (compare(a, b) <= 0) {
-        yield a;
-        ai++;
-      } else {
-        yield b;
-        bi++;
-      }
-    }
-
-    // remainder
-    while (ai < as.length) yield as[ai++];
-    while (bi < bs.length) yield bs[bi++];
-  },
-};
-
-type INonNullFn<K extends keyof ITypeMap, Fn extends keyof IFns<K>> = IFnsType<
-  Exclude<ITypeMap[K], null>
->[Fn];
+type INonNullFn<
+  K extends keyof ITypeMap,
+  Fn extends keyof IFnsType<K>,
+> = IFnsType<Exclude<ITypeMap[K], null>>[Fn];
 
 type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>;
 
@@ -137,7 +96,7 @@ type IValueFnsArgs<K extends keyof ITypeMap> = Optional<
   "_or" | "_and" | "_cmp" | "_string" | "_json"
 >;
 
-const UnitFns = <K extends keyof ITypeMap>(
+const unitFns = <K extends keyof ITypeMap>(
   key: K,
   argsFn: () => Partial<IFns<K>> & IValueFnsArgs<K>,
 ): IFns<K> & IValueFns<K> => {
@@ -154,7 +113,7 @@ const UnitFns = <K extends keyof ITypeMap>(
     args._cmp || ((a, b) => (_compare(a, b) === 0 ? Cmp.Equal : Cmp.Disjoint));
   const _compare = args._compare;
   const _string = args._string || String;
-  const _json = args._json || ((value) => value);
+  const _json = args._json || ((value) => value as json);
 
   return {
     or(a, b) {
@@ -195,113 +154,161 @@ const UnitFns = <K extends keyof ITypeMap>(
   };
 };
 
-export const NullFns: IFns<"null"> = {
+export const NullFns = baseFns<"null">(() => ({
   or: () => [null],
   and: () => [null],
   cmp: () => Cmp.Equal,
   compare: () => 0,
   string: () => "null",
   json: () => null,
-};
+}));
 
-export const NumberFns = UnitFns("number", () => ({
+export const NumberFns = unitFns("number", () => ({
   _compare: (a: number, b: number) => (a === b ? 0 : a < b ? -1 : 1),
 }));
 
-export const StringFns = UnitFns("string", () => ({
+export const StringFns = unitFns("string", () => ({
   _compare: (a: string, b: string) => a.localeCompare(b),
 }));
 
-export const ArrayFns: IFns<"array"> = {
-  or(a, b) {
-    throw "TODO";
-  },
-  and(a, b) {
-    throw "TODO";
-  },
-  cmp(a, b) {
-    throw "TODO";
-  },
-  compare(a, b) {
-    throw "TODO";
-  },
-  string(value) {
-    throw "TODO";
-  },
-  json(value) {
-    throw "TODO";
-  },
-};
+export const ArrayFns = baseFns<"array">(() => {
+  const { union, intersect } = UtilFns;
+  const { _or: _union, _and: _intersect, _compare } = NumberFns;
+  const unionIdx = (as: number[], bs: number[]) =>
+    union(as, bs, _union, _compare);
 
-export const AnyFns: IFns<"null"> = {
-  or(a, b) {
-    throw "TODO";
-  },
-  and(a, b) {
-    throw "TODO";
-  },
-  cmp(a, b) {
-    throw "TODO";
-  },
-  compare(a, b) {
-    throw "TODO";
-  },
-  string(value) {
-    throw "TODO";
-  },
-  json(value) {
-    throw "TODO";
-  },
-};
+  return {
+    or(a, b) {
+      throw "TODO";
+    },
+    and(a, b) {
+      throw "TODO";
+    },
+    cmp(a, b) {
+      throw "TODO";
+    },
+    compare(a, b) {
+      throw "TODO";
+    },
+    string(value) {
+      throw "TODO";
+    },
+    json(value) {
+      throw "TODO";
+    },
+  };
+});
 
-export const NeverFns: IFns<"never"> = {
-  or(as, bs): [string[]] {
-    return [[...UtilFns.union(as, bs, StringFns._or, StringFns._compare)]];
-  },
-  and(as, bs) {
-    return [[...UtilFns.intersect(as, bs, StringFns._and, StringFns._compare)]];
-  },
-  cmp(a, b) {
-    throw "TODO";
-  },
-  compare(a, b) {
-    throw "TODO";
-  },
-  string(value) {
-    throw "TODO";
-  },
-  json(value) {
-    throw "TODO";
-  },
-};
-
-export const UnionFns: IFns<"union"> = {
-  or(a, b) {
-    throw "TODO";
-  },
-  and(a, b) {
-    throw "TODO";
-  },
-  cmp(a, b) {
-    throw "TODO";
-  },
-  compare(a, b) {
-    throw "TODO";
-  },
-  string(value) {
-    throw "TODO";
-  },
-  json(value) {
-    throw "TODO";
-  },
-};
+export const ObjectFns = baseFns<"object">(() => {
+  return {
+    or(a, b) {
+      throw "TODO";
+    },
+    and(a, b) {
+      throw "TODO";
+    },
+    cmp(a, b) {
+      throw "TODO";
+    },
+    compare(a, b) {
+      throw "TODO";
+    },
+    string(value) {
+      throw "TODO";
+    },
+    json(value) {
+      throw "TODO";
+    },
+  };
+});
 
 export const Fns = {
-  never: NeverFns,
   null: NullFns,
   number: NumberFns,
   string: StringFns,
-  util: UtilFns,
+  array: ArrayFns,
+  object: ObjectFns,
+};
+
+const fns = <K extends keyof typeof Fns>(key: K): IFnsType<unknown> => Fns[key];
+
+export const UtilFns = {
+  *intersect<V>(
+    as: V[],
+    bs: V[],
+    and: (a: V, b: V) => [] | [V],
+    compare: (a: V, b: V) => number,
+  ) {
+    let ai = 0;
+    let bi = 0;
+
+    while (ai < as.length && bi < bs.length) {
+      const a = as[ai];
+      const b = bs[bi];
+      const intersection = and(a, b);
+
+      // merged
+      if (intersection.length === 1) {
+        yield intersection[0];
+        ai++;
+        bi++;
+        continue;
+      }
+
+      // disjoint
+      if (compare(a, b) < 0) {
+        ai++;
+      } else {
+        bi++;
+      }
+    }
+  },
+  *union<V>(
+    as: V[],
+    bs: V[],
+    or: (a: V, b: V) => [V] | [V, V],
+    compare: (a: V, b: V) => number,
+  ) {
+    let ai = 0;
+    let bi = 0;
+
+    while (ai < as.length && bi < bs.length) {
+      const a = as[ai];
+      const b = bs[bi];
+      const union = or(a, b);
+
+      // merged
+      if (union.length === 1) {
+        yield union[0];
+        ai++;
+        bi++;
+        continue;
+      }
+
+      // disjoint
+      if (compare(a, b) < 0) {
+        yield a;
+        ai++;
+      } else {
+        yield b;
+        bi++;
+      }
+    }
+
+    // remainder
+    while (ai < as.length) yield as[ai++];
+    while (bi < bs.length) yield bs[bi++];
+  },
+  compare<V>(as: V[], bs: V[], compareFn: (a: V, b: V) => number): number {
+    const len = Math.min(as.length, bs.length);
+
+    for (let i = 0; i < len; i++) {
+      const sort = compareFn(as[i], bs[i]);
+      if (sort !== 0) return sort;
+    }
+
+    return as.length - bs.length;
+  },
 };
 
 export const enum Cmp {
